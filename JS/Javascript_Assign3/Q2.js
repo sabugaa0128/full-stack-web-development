8fa32b2d2c2e66a43e6e41e68682f3ed55d2f3e0
sabugaa0128 <sabugaarozvelt@gmail.com>
2023-12-11 15:40:29 +0530

    3 submitted

---
+++ JS/Javascript_Assign3/Q2.js
@@ -0,0 +1,12 @@
+let score = 85;
+
+if (score >= 90) {
+    console.log("A grade");
+} else if (score <= 90 && score >= 70) {
+    console.log("B grade");
+} else if (score <= 70 && score >= 50) {
+    console.log("C grade");
+} else if(score <= 50){
+    console.log("F grade");
+}
+
